f802de40be00c36da1b08ea5a4fa834d35d32504
ashwiniethiraj3 <ashwiniethiraj@gmail.com>
2025-08-14 19:36:38 +0530

    second commit

--- src/main/java/com/example/demo/DemoforgithubactionsApplication.java
+++ src/main/java/com/example/demo/DemoforgithubactionsApplication.java
@@ -11,7 +11,7 @@ public class DemoforgithubactionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "welcome";
+		return "welcome to githubbbb";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoforgithubactionsApplication.class, args);
